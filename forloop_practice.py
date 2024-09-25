8d9d84eb5fef129c7066ccd393fda328d564cc36
itsshivamdwivedi <88644787+itsshivamdwivedi@users.noreply.github.com>
2024-09-25 07:23:01 +0000

    commit

--- forloop_practice.py
+++ forloop_practice.py
@@ -5,3 +5,21 @@ lengths=[]
 for word in words:
     lengths.append(len(word))
 print(lengths)
+
+
+#  Write a program to print the following pattern
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5 
+
+#  let's define the number of rows
+rows=5
+for i in range(1,rows + 1,1):
+
+    for j in range(1,i+1):          #inner loop 
+        print(j, end=" ")
+
+    print(" ")
+
