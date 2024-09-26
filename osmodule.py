f98949caabb454bd4a79d5e35220d4e13dff1fe7
iamshivamdwivedi410 <iamshivamdwivedi410@gmail.com>
2024-09-27 00:34:07 +0530

    commit

--- osmodule.py
+++ osmodule.py
@@ -1,10 +1,9 @@
 # This code shows how to make many folder in just a seconds
-# import os
-# if(not os.path.exists("data")):
-#  os.mkdir("data")
+import os
+if(not os.path.exists("data")):
+ os.mkdir("data")
 
-#  for i in range(0,100):
-#      os.mkdir(f"data/Day{i+1}")
-# import module1
-# print(module1.sum(10,20))
-from module1 import sum  
+for i in range(0,100):
+    os.mkdir(f"data/Day{i+1}")
+# we can use the os module to automate the tasks in python
+
