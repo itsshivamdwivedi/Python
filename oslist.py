2ad8fa544c7c2f7471afd8857c911607e8cbc968
itsshivamdwivedi <88644787+itsshivamdwivedi@users.noreply.github.com>
2024-09-26 19:56:26 +0000

    os module commit

--- oslist.py
+++ oslist.py
@@ -1,5 +1,11 @@
 import os
-folders =os.listdir("data")
+folders =os.listdir("data")    # list of the directories
 print(folders)
-for folders in folders:
-    print(folders)
+# for folders in folders:
+#     print(folders)
+
+
+# To display the files of all the folder we can use a loop 
+for folder in folders:
+    print(os.listdir(f"/data/{folder}"))
+    
--- osmodule.py
+++ osmodule.py
@@ -1,9 +1,33 @@
 # This code shows how to make many folder in just a seconds
-import os
-if(not os.path.exists("data")):
- os.mkdir("data")
+# import os
+# try:
+#      if(not os.path.exists("data")):
+#        os.mkdir("data")
 
-for i in range(0,100):
-    os.mkdir(f"data/Day{i+1}")
+#      for i in range(0,100):
+#          os.mkdir(f"data/Day{i+1}")
+# except Exception as e:
+#       print(f"An error occurred: {e}")
+#       print("We are going to learn the os module in detail")
+
+
+# In the above code i have used the try except exception to run the code of next line even if the error 
+# occured in the previous line
+
+# We  use mkdir() to make the new directory
+# We use the path.exists method to check whether the path exists or not 
+
+# import os
+# try:
+#      if(not os.path.exists("data")):
+#        os.mkdir("data")
+
+#      for i in range(0,100):
+#          old_name=f"data/Day{i+1}"
+#          new_name=f"data/Python{i+1}"
+#          os.rename(old_name,new_name)         #this is how we can change the names of the files in the directories 
+# except Exception as e:
+#       print(f"An error occurred: {e}")
+#       print("We are going to learn the os module in detail")
 
 
