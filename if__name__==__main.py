896ad2a879bf92d7e1f8747ee07c948483dc5624
itsshivamdwivedi <88644787+itsshivamdwivedi@users.noreply.github.com>
2024-09-26 10:45:17 +0000

    commit

---
+++ if__name__==__main.py
@@ -0,0 +1,6 @@
+import shivam
+shivam.welcome()
+
+
+# if we are importing this module shivam so function welcome in the shivam.py is getting executed automatically
+# to avoid this problem we can use if__name__ =="__main__"
--- shivam.py
+++ shivam.py
@@ -1,7 +1,13 @@
+
+
+
+
 def welcome():
  print("you are welcome my friend ")
 
 shivam= "good boy"
 print(__name__)
-if __name__="__main__":
+if __name__=="__main__":
     welcome()
+
+# if __name__=="__main__":   By using these basically we are implementing a check to know from where the files are getting executed
